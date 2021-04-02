6a5f881f73a9ee7718188f219285f5861ac02070
gungun <2378710901@qq.com>
2021-04-02 14:19:07 +0800

    封装网络请求

---
+++ src/network/request.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+export function request(config){
+
+  const instance =axios.create({
+      baseURL:'http://123.207.32.32:8000',
+      timeout:5000,
+    });
+  //2.axios的拦截器
+  //2.1请求拦截的作用
+  instance.interceptors.request.use(config=>{
+    return config //拦截之后要发送出去
+  },err =>{
+    console.log(err);
+  });
+
+  instance.interceptors.response.use(res =>{
+    //响应的结果
+    return res.data //拦截之后放行
+  },err =>{
+    //失败的结果
+  })
+
+  //3.发送真正的网络请求
+  return instance(config);
+}
