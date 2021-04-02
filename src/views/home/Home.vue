248fc3ff58fff7626143e7c8f35a39bcad9d9afa
gungun <2378710901@qq.com>
2021-04-02 14:49:52 +0800

    请求首页的多个数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,10 +8,27 @@
 
 <script>
 import NavBar from 'components/common/navbar/NavBar';
+import {getHomeMultidata} from 'network/home'
 export default {
   name:'Home',
+  data(){
+    return {
+      banners:[],
+      recommends:[],
+    }
+  },
   components:{
     NavBar,
+  },
+  created(){
+    //组件创建完之后发送网络请求
+    //1.请求数据
+    getHomeMultidata().then(res =>{
+      //拿到返回的数据
+      this.banners = res.data.banner.list //将数据保存到data
+      this.recommends = res.data.recommend.list
+    })
+    
   }
 }
 </script>
