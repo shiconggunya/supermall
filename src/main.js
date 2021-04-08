a24513c262d8486486b48c9425d64d6f600fb7ea
gungun <2378710901@qq.com>
2021-04-08 10:56:16 +0800

    事件总线

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
 
+Vue.prototype.$bus = new Vue();   //在原型上添加事件总线
+
 new Vue({
   render : h=>h(App),
   router,
