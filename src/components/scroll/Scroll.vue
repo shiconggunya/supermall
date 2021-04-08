a3d5c80583c9f2c075f3821715522e543ca626c2
gungun <2378710901@qq.com>
2021-04-08 10:58:17 +0800

    封装 better-score

--- src/components/scroll/Scroll.vue
+++ src/components/scroll/Scroll.vue
@@ -10,20 +10,19 @@
 import BScroll from "better-scroll";
 export default {
   name: "Scroll",
-  props:{
-    probeType:{
-      type:Number,
-      default(){
+  props: {
+    probeType: {
+      type: Number,
+      default() {
         return 0;
-      }
+      },
     },
-    pullUpLoad:{
+    pullUpLoad: {
       typeof: Boolean,
-      default(){
+      default() {
         return false;
-      }
-    }
-
+      },
+    },
   },
   data() {
     return {
@@ -33,34 +32,43 @@ export default {
   mounted() {
     //1.创建Bscroll对象
     this.scroll = new BScroll(this.$refs.wrapper, {
-      click:true, //开启事件监听
-      probeType: this.probeType , //实时滚动监听
-      pullUpLoad:this.pullUpLoad, //下拉加载更多
+      click: true, //开启事件监听
+      probeType: this.probeType, //实时滚动监听
+      pullUpLoad: this.pullUpLoad, //下拉加载更多
     });
 
     //2.实时监听滚动位置
-    this.scroll.on('scroll',(position) => {
-      //将滚动的位置发送出去
-      this.$emit('scroll',position);
-    })
+    if (this.probeType === 2 || this.probeType === 3) {
+      this.scroll.on("scroll", (position) => {
+        //将滚动的位置发送出去
+        this.$emit("scroll", position);
+      });
+    }
 
-    //3.监听上拉事件
-    this.scroll.on('pullingUp',() => {
-     this.$emit('pullingUp') //发射事件告诉别人可以加载更多
-    })
-
+    //3.监听下拉加载更多
+    if(this.pullUpLoad){
+      this.scroll.on('pullingUp',() => {
+        this.$emit('pullingUp');
+      })
+    }
   },
   methods: {
     //自己封装的scrollTo
     //调用scroll里面的scrollTo(),第三个参数是时间
     scrollTo(x, y, time = 500) {
-      this.scroll.scrollTo(x, y, time);
+      this.scroll && this.scroll.scrollTo(x, y, time);
     },
 
     //自己封装的finishPullUp
-    finishPullUp(){
-      this.scroll.finishPullUp();
-    }
+    finishPullUp() {
+      this.scroll && this.scroll.finishPullUp(); //先判断有没有this.scroll,这个对象,在执行
+    },
+
+    //加载完刷新
+    refresh() {
+      console.log("-----");
+      this.scroll && this.scroll.refresh();
+    },
   },
 };
 </script>
